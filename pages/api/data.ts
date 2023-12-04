61aa4c08a9d39c809246273ee55d058d8af6b0ff
zjy4fun <zjy4fun@gmail.com>
2023-12-05 00:04:46 +0800

    add @ast-grep/cli

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -10,7 +10,8 @@ const messageApi = (req: NextApiRequest, res: NextApiResponse) => {
     "@antfu/ni",
     "x-init",
     "corepack",
-    "@jsdevtools/version-bump-prompt"
+    "@jsdevtools/version-bump-prompt",
+    "@ast-grep/cli"
   ]);
 };
 
